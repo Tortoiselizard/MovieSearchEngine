a15d6a75168fef2ae3c61930804c66768c6534da
Troy Pernía <troy00pernia@gmail.com>
2025-07-16 11:31:25 -0400

    feat(MSE-10): add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from './components/Home/Home'
+import Header from './components/Header/Header'
 import './App.css'
 
 function App () {
   return (
     <>
+      <Header />
       <Home />
     </>
   )
