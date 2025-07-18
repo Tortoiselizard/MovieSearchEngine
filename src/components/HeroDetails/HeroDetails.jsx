2306a0b0b42f9a4bf7a5ab72bae6d3a951bb39dc
Troy Pernía <troy00pernia@gmail.com>
2025-07-18 18:46:40 -0400

    feat(MSE-16): add styles to background of HeroDetails

--- src/components/HeroDetails/HeroDetails.jsx
+++ src/components/HeroDetails/HeroDetails.jsx
@@ -6,36 +6,43 @@ const { VITE_API_IMAGE_URL } = import.meta.env
 
 export default function MovieCardDetails ({ movie }) {
   return (
-    <div className={styles.hero}>
-      <div className={styles.heroBackground}>
-        <img src={`${VITE_API_IMAGE_URL}/w1280/${movie.backdrop_path}`} alt={movie.title} />
-      </div>
-      <div className={styles.heroContent}>
-        <div className={styles.heroInfo}>
-          <h1 className={styles.title}>{movie.title}</h1>
+    <div className={styles.container}>
+      <div className={styles.hero}>
+        <div className={styles.heroBackground}>
+          <img
+            src={`${VITE_API_IMAGE_URL}/w1280/${movie.backdrop_path}`}
+            alt={movie.title}
+            className={styles.backgroundImage}
+          />
+          <div className={styles.heroGradient} />
+        </div>
+        <div className={styles.heroContent}>
+          <div className={styles.heroInfo}>
+            <h1 className={styles.title}>{movie.title}</h1>
 
-          <div className={styles.metadata}>
-            <span className={styles.rating}>{movie.vote_average}</span>
-            <span className={styles.year}>{movie.release_date.split('-')[0]}</span>
-          </div>
+            <div className={styles.metadata}>
+              <span className={styles.rating}>{movie.vote_average}</span>
+              <span className={styles.year}>{movie.release_date.split('-')[0]}</span>
+            </div>
 
-          <p className={styles.description}>{movie.overview}</p>
+            <p className={styles.description}>{movie.overview}</p>
 
-          <div className={styles.genres}>
-            {
+            <div className={styles.genres}>
+              {
               movie.genres.map(({ name, id }) => (
                 <span key={id} className={styles.genre}>{name}</span>
               ))
             }
+            </div>
           </div>
-        </div>
 
-        <div className={styles.heroPoster}>
-          <img
-            src={`${VITE_API_IMAGE_URL}/w500/${movie.poster_path}`}
-            alt={'image: ' + movie.title}
-            className={styles.posterImage}
-          />
+          <div className={styles.heroPoster}>
+            <img
+              src={`${VITE_API_IMAGE_URL}/w500/${movie.poster_path}`}
+              alt={'image: ' + movie.title}
+              className={styles.posterImage}
+            />
+          </div>
         </div>
       </div>
     </div>
