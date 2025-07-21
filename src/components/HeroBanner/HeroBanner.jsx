f10ebae3cd3e996a80a3291b747d9fdb6e5adfe4
Troy Pernía <troy00pernia@gmail.com>
2025-07-21 16:23:41 -0400

    feat(MSE-20): add styles to HeroBanner for UI responsive

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -2,11 +2,31 @@ import { Link } from 'react-router'
 
 import { useMyContext } from '../../context/MyContext.jsx'
 import styles from './HeroBanner.module.css'
+import { useEffect, useState } from 'react'
 
 export default function HeroBanner () {
   const { VITE_API_IMAGE_URL } = import.meta.env
   const { state: globalState } = useMyContext()
   const movie = globalState.movies.list[0]
+  const [windowWidth, setWindowWith] = useState(window.innerWidth)
+
+  // Breakpoints
+  const isMobile = windowWidth < 768
+  const isTable = windowWidth >= 768 && windowWidth < 1024
+  const isDesktop = windowWidth >= 1024
+
+  // Update windowWidth when change viewport width
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  function handleResize () {
+    setWindowWith(window.innerWidth)
+  }
 
   return (
     <section className={styles.hero}>
@@ -18,7 +38,7 @@ export default function HeroBanner () {
         </div>
       </div>
       <div className={styles.heroImage}>
-        <img src={`${VITE_API_IMAGE_URL}/w1280/${movie.backdrop_path}`} alt={movie.title} />
+        <img src={`${VITE_API_IMAGE_URL}/w1280/${isMobile ? movie.poster_path : movie.backdrop_path}`} alt={movie.title} />
       </div>
       <div className={styles.fadeBottom} />
     </section>
