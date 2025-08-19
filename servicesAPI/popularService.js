7eb8c288b9487a3675950d9f6e349d4518250b2e
Troy Pernía <troy00pernia@gmail.com>
2025-08-19 16:38:38 -0400

    fix(MSE-76): fix request to popular

--- servicesAPI/popularService.js
+++ servicesAPI/popularService.js
@@ -15,16 +15,30 @@ export class PopularService {
       results: [],
       lastMovie: filters.lastMovie
     }
+
+    let lastRequest = []
+    let page = filters.page
+    let totalPages
     // Do request to pages
-    for (let page = filters.page; moviePackage.results.length < filters.moviesPerPage; page++) {
-      const { results } = await this.#popularRepository.getPopularMovies({ page })
+    do {
+      const { results, total_pages } = await this.#popularRepository.getPopularMovies({ page })
+      totalPages = total_pages
+      lastRequest = results
       moviePackage.results.push(...results.slice(moviePackage.lastMovie))
       moviePackage.page = page
-    }
+      moviePackage.lastMovie = 0
+      page++
+    } while ((moviePackage.results.length < filters.moviesPerPage) && (page <= totalPages))
 
     if (moviePackage.results.length !== filters.moviesPerPage) {
       moviePackage.results = moviePackage.results.slice(0, filters.moviesPerPage)
-      moviePackage.lastMovie = filters.moviesPerPage
+      const lastMovie = moviePackage.results[moviePackage.results.length - 1]
+      const indexMovie = lastRequest.findIndex(({ id }) => id === lastMovie.id) + 1
+      moviePackage.lastMovie = indexMovie
+    }
+
+    if (page > totalPages) {
+      moviePackage.lastPage = true
     }
 
     return moviePackage
