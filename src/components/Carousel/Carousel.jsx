10b9f57b73ba757c56c9f5a3af4434cb364cab80
Troy Pernía <troy00pernia@gmail.com>
2025-08-28 14:06:38 -0400

    fix(MSE-112): fix right button of paginator

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -8,7 +8,7 @@ import styles from './Carousel.module.css'
 export default function Carousel ({ items, title, seeMore, id, children, imageSize }) {
   const [position, setPosition] = useState(0)
   const itemsContainer = useRef()
-  const [scrollButtonRightVisibility, setScrollButtonRightVisibility] = useState(true)
+  const [scrollButtonRightVisibility, setScrollButtonRightVisibility] = useState(false)
   const [countCardVisibles, setCountCardVisibles] = useState(0)
 
   // Initialice horizontal scrollbar in 0
@@ -23,15 +23,20 @@ export default function Carousel ({ items, title, seeMore, id, children, imageSi
     const elementRect = itemsContainer.current.childNodes[0].getBoundingClientRect()
     const n = Math.floor(containerRect.width / (elementRect.width + 8))
     setCountCardVisibles(n)
+
+    // Update scrollButtonRightVisibility when the list is very short
+    if (itemsContainer.current.childNodes.length < n) return
+    setScrollButtonRightVisibility(true)
   }, [itemsContainer.current])
 
   function scroll (direction) {
     if (!itemsContainer.current) return
     const firstElementRect = itemsContainer.current.childNodes[0].getBoundingClientRect()
 
-    const newPosition = direction === 'left'
+    let newPosition = direction === 'left'
       ? Math.max(0, position - countCardVisibles)
       : Math.min(itemsContainer.current.childNodes.length - countCardVisibles, position + countCardVisibles)
+    newPosition = newPosition < 0 ? 0 : newPosition
 
     const targetElement = itemsContainer.current.childNodes[newPosition]
     const elementRect = targetElement.getBoundingClientRect()
@@ -47,12 +52,17 @@ export default function Carousel ({ items, title, seeMore, id, children, imageSi
     <div className={styles.carouselContainer}>
       <div className={styles.carouselTitle}>
         <h2 className={styles.title}>{title}</h2>
-        <button className={styles.more} onClick={seeMore}>See more</button>
+        <button
+          className={styles.more}
+          onClick={seeMore}
+          style={{ visibility: scrollButtonRightVisibility ? '' : 'hidden' }}
+        >See more
+        </button>
       </div>
       <div id={id} className={styles.rowItemsContainer}>
         <button
           className={`${styles.scrollButton} ${styles.scrollLeft}`}
-          style={{ opacity: position > 0 ? 1 : 0 }}
+          style={{ visibility: position > 0 ? '' : 'hidden' }}
           onClick={() => { scroll('left') }}
         >
           <ChevronLeft className={styles.scrollIcon} />
@@ -70,7 +80,7 @@ export default function Carousel ({ items, title, seeMore, id, children, imageSi
         </div>
         <button
           className={`${styles.scrollButton} ${styles.scrollRight}`}
-          style={{ opacity: scrollButtonRightVisibility ? 1 : 0 }}
+          style={{ visibility: scrollButtonRightVisibility ? '' : 'hidden' }}
           onClick={() => { scroll('right') }}
         >
           <ChevronRight className={styles.scrollIcon} />
