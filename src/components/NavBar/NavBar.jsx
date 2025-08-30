2e1bcbd809b74a8b4af4daaf7c93b1c32923014b
Troy Pernía <troy00pernia@gmail.com>
2025-08-30 16:51:51 -0400

    feat(MSE-98): add mark in active navbar options

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,35 @@
 import { Link, useLocation } from 'react-router'
 
 import styles from './NavBar.module.css'
+import { useEffect, useState } from 'react'
 
 export default function NavBar () {
   const { pathname } = useLocation()
+  const [optionActive, setOptionActive] = useState('popular')
   const replace = pathname === '/notFound'
+
+  useEffect(() => {
+    if (optionActive === pathname) return
+    const newOptionActive = pathname
+    setOptionActive(newOptionActive)
+  }, [pathname])
+
   return (
     <nav className={styles.navBarContainer}>
-      <Link to='/' className={styles.navLink} replace={replace}>Popular</Link>
-      <Link to='/favorites' className={styles.navLink} replace={replace}>Favorites</Link>
+      <Link
+        to='/'
+        className={`${styles.navLink} ${optionActive === '/' ? styles.active : ''}`}
+        replace={replace}
+      >
+        Popular
+      </Link>
+      <Link
+        to='/favorites'
+        className={`${styles.navLink} ${optionActive === '/favorites' ? styles.active : ''}`}
+        replace={replace}
+      >
+        Favorites
+      </Link>
     </nav>
   )
 }
